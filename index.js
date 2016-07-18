e595e6cdde394972c298159574919a08dc8da442
Mauricio Andrades <mod.and.op@gmail.com>
2016-07-17 22:12:44 -0700

    added promise scraping

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var writeFile = Promise.promisify(require('fs').writeFile);
 var cheerio = require('cheerio');
 var toMarkdown = require('to-markdown');
 
-
 var md = toMarkdown('<h1>Hello world!</h1>');
 
 var urls = ['https://developer.mozilla.org/en-US/docs/Web/HTML/Element/applet']
@@ -15,45 +14,51 @@ for (var i = 0; i < urls.length; i++) {
   promises.push(request.getAsync(urls[i]));
 }
 
-
-Promise.all(promises).then(function(data){
+Promise.all(promises).then(function(data) {
   var parsed = JSON.parse(JSON.stringify(data));
   var $ = cheerio.load(parsed[0]['body']);
+
   var html = $('#wikiArticle').html();
 
-  console.log(toMarkdown(html, {converters: [
-    {
+  console.log(toMarkdown(html, {
+    converters: [{
       filter: 'code',
       replacement: function(content) {
         return '`' + content + '`';
       }
-    },
-    {
+    }, {
       filter: function(node) {
         return node.className === "brush: html";
       },
-      replacement: function (content) {
-        return '```html' + '\n' + content + '\n' +'```';
+      replacement: function(content) {
+        return '```html' + '\n' + content + '```';
       }
-    },
-    {
+    }, {
       filter: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
 
       replacement: function(innerHTML, node) {
         var hLevel = node.tagName.charAt(1);
         var hPrefix = '';
-        for(var i = 0; i < hLevel; i++) {
+        for (var i = 0; i < hLevel; i++) {
           hPrefix += '#';
         }
         return '\n' + hPrefix + ' ' + innerHTML + '\n\n';
       }
-    }
-  ]}))
+    }, {
+      filter: function(node) {
+        return node.className === 'note';
+      },
+      replacement: function(content) {
+        return "" + content;
+      }
+    }],
+    gfm: true
+  }))
 
-  $('#wikiArticle').contents().each(function(){
-    console.log($(this).text().trim())
+  $('#wikiArticle').contents().each(function() {
+    var text = ($(this).text().trim())
   })
 
-}).catch(function (err) {
-  if(err)console.log(err);
+}).catch(function(err) {
+  if (err) console.log(err);
 })
