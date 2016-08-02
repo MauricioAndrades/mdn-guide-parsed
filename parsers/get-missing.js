e7aae6bd90ca74dc68c26eaf434102915c2d33f3
Mauricio Andrades <mod.and.op@gmail.com>
2016-08-02 11:05:51 -0700

    added logic to fill missing entries

---
+++ parsers/get-missing.js
@@ -0,0 +1,57 @@
+var requireg = require('/Users/Op/.npm-global/lib/node_modules/requireg/lib/requireg.js');
+var tlog = requireg('mod-log').tlog;
+var Promise = requireg('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var request = Promise.promisifyAll(requireg('request'));
+var cheerio = requireg('cheerio');
+var traverse = Promise.promisifyAll(requireg('traverse'));
+var jsondata = JSON.parse(fs.readFileSync('/Users/Op/Documents/gschool/proj/mdn-guide/guides/Element.json', 'utf8'));
+var arr = [];
+
+var traverseAsync = function (data) {
+	return new Promise(function (resolve, reject) {
+		try {
+			traverse(data).forEach(function (x) {
+				if (x.desc && x.desc === 'Editorial review completed.') {
+					arr.push({
+						key: this.key,
+						node: this.node
+					});
+				}
+			})
+			resolve(arr);
+		} catch (e) {
+			reject(e);
+		}
+	});
+};
+
+traverseAsync(jsondata).then(function (data) {
+	var promises = [];
+	data.forEach(function (el, i) {
+		promises.push(request.getAsync(el.node.href))
+	});
+	return Promise.all(promises);
+}).then(function (data) {
+	var $;
+	var descriptions = [];
+	data.forEach(function (el, i) {
+		return Promise.try(function () {
+			$ = cheerio.load(el.body);
+			var description = $('#wikiArticle > p').text();
+			descriptions.push(description);
+		})
+	})
+	return descriptions;
+}).then(function (data) {
+	data.forEach(function (el, i) {
+		arr[i].node.desc = data[i];
+	})
+	arr.forEach(function (el, i) {
+		var key = eval(el.key);
+		jsondata[key] = el.node;
+	})
+	console.log(JSON.parse(JSON.stringify(jsondata)))
+}).catch(function (e) {
+	console.log(e);
+});
--- traverse-jsonfileter.js
+++ traverse-jsonfileter.js
@@ -5,23 +5,37 @@ var fs = require('fs');
 var data = JSON.stringify(fs.readFileSync('/Volumes/ramdisk/interfaces-domsjon.json', 'utf8'));
 
 // var data = JSON.parse(data);
-var tlog = require('mod-log').tlog;
-var plog = require('mod-log').plog;
+var tlog = requireg('mod-log').tlog;
+var plog = requireg('mod-log').plog;
 
 var scrubbed = traverse(JSON.parse(data)).map(function (x) {
   if (x.circular) x.remove();
 });
 
 function traversedomjson(_data, callback) {
+  var opts = {
+    clean: true
+  }
   var acc = [];
-  var leaves = traverse(JSON.parse(_data)).reduce(function (acc, x) {
-    if (x.tagName !== undefined) {
-      if (x.tagName.toString() === "A") {
+  var leaves = traverse(JSON.parse(_data)).map(function (acc, x) {
+    if (opts.clean) {
+      if (this.circular) this.remove();
+      var key = this.key;
+      if (this.key !== undefined && typeof this.key === 'string') {
+        if (this.key) {
+          var j = JSON.stringify(this.key);
+          console.log('=========');
+          console.log(j)
+        }
+      }
+    }
+    if (this.tagName !== undefined) {
+      if (x.tagName.toString() === "A" || x.tagName.toString() === "DD") {
         var obj = {};
         if (x.text) obj.name = x.text;
         if (x.title) obj.desc = x.title;
         if (x.href) obj.href = x.href;
-        acc.push(obj);
+        if (Object.keys(obj).length > 0) acc.push(obj);
       }
     }
     return acc;
